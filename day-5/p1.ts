759871fd96aa7512bef4ebd7bd614d6497b0b332
Joachim Toft <joachimt@spotify.com>
2019-12-05 23:24:46 +0100

    day5

---
+++ day-5/p1.ts
@@ -0,0 +1,67 @@
+const fs = require("fs");
+
+type Opcode = "01" | "02" | "03" | "04" | "99";
+
+const runProgram = (program: string[]) => {
+  let pointer: number = 0;
+  let shouldExit: boolean = false;
+
+  const readParam = (input: string, mode?: string): number => {
+    if (mode === "0") return Number(program[Number(input)]);
+    return Number(input);
+  };
+
+  while (true) {
+    const instruction = program[pointer].toString().padStart(5, "0");
+    const opcode = instruction.slice(3, 5);
+    const [m3, m2, m1] = instruction.split("");
+
+    switch (opcode) {
+      case "01":
+        const i1 = readParam(program[pointer + 1], m1);
+        const i2 = readParam(program[pointer + 2], m2);
+        const writePos = readParam(program[pointer + 3]);
+
+        const result = i1 + i2;
+        program[writePos] = String(result);
+        pointer += 4;
+        break;
+      case "02": {
+        const i1 = readParam(program[pointer + 1], m1);
+        const i2 = readParam(program[pointer + 2], m2);
+        const writePos = readParam(program[pointer + 3]);
+
+        const result = i1 * i2;
+        program[writePos] = String(result);
+        pointer += 4;
+        break;
+      }
+      case "03": {
+        const input = 1;
+        const writePos = readParam(program[pointer + 1]);
+
+        program[writePos] = String(input);
+        pointer += 2;
+        break;
+      }
+      case "04": {
+        const output = readParam(program[pointer + 1], m1);
+
+        console.log(output);
+        pointer += 2;
+        break;
+      }
+      case "99":
+      default: {
+        shouldExit = true;
+        break;
+      }
+    }
+
+    if (shouldExit) break;
+  }
+};
+
+const input = fs.readFileSync("day-5/input", "utf8");
+const programInput = input.split(",");
+runProgram(programInput);
